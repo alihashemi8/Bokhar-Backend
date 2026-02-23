f0ae5c365c8360f91a5a4c3d541190c30563cb51
ali <alihashemi5553@gmail.com>
2026-02-24 01:21:29 +0330

    Auth with password

--- users/authenticate.py
+++ users/authenticate.py
@@ -1,6 +1,9 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
@@ -26,3 +29,21 @@ class CookieJWTAuthentication(JWTAuthentication):
                 return None
 
         return self.get_user(validated_access), validated_access
+
+
+User = get_user_model()
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, request, phone=None, password=None, **kwargs):
+        if not phone or not password:
+            return None
+
+        try:
+            user = User.objects.get(phone=phone)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+
+        return None
