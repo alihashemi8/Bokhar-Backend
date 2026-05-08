0fdf0ab13bf0ab6feeffa564b073c8f05aa0ae86
hasti2 <hastimanooch84@gmail.com>
2026-05-08 10:05:16 +0330

    add rushfeesetting , pickuptemplate , deliverytemplate serializer

--- order/serializers.py
+++ order/serializers.py
@@ -4,9 +4,58 @@ from rest_framework import serializers
 from .models import *
 
 # serializers.py
+#قیمت و درصد برای سفارش های فوری وارد میکند
+class RushFeeSettingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RushFeeSetting
+        fields = '__all__'
+#ظرفیت برای تحویل گرفتن مشخص میکنه
+class PickupTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PickUpTemplate
+        fields = '__all__'
+
+class DeliveryTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryTemplate
+        fields = '__all__'
+
+class UpdateRushFeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RushFeeSetting
+        fields = '__all__'
+
+        def update(self, instance, validated_data):
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+
+            instance.save()
+            return instance
+
+class UpdatePickupTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PickUpTemplate
+        fields = '__all__'
+
+        def update(self, instance, validated_data):
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+
+            instance.save()
+            return instance
 
 
+class UpdateDeliveryTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryTemplate
+        fields = '__all__'
 
+        def update(self, instance, validated_data):
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+
+            instance.save()
+            return instance
 
 #سفارشا وقتی که داخل سبد خرید هست نشون بده
 class OrderCartItemSerializer(serializers.Serializer):
--- order/urls.py
+++ order/urls.py
@@ -27,5 +27,27 @@ urlpatterns = [
     path('orders/<int:order_id>/status-history/',OrderStatusHistoryView.as_view(), name='order-status-history'),
 
     path("products/search/", SearchOrderView, name="product-search"),
+    path('rush-fee-settings/',
+         RushFeeSettingListCreateView.as_view(),
+         name='rush-fee-list-create'),
+    path('rush-fee-settings/<int:pk>/',
+         RushFeeSettingDetailView.as_view(),
+         name='rush-fee-detail'),
+
+    # Pickup Times
+    path('api/pickup-times/',
+         PickupTimeListCreateView.as_view(),
+         name='pickup-time-list-create'),
+    path('api/pickup-times/<int:pk>/',
+         PickupTimeDetailView.as_view(),
+         name='pickup-time-detail'),
+
+    # Delivery Times
+    path('delivery-times/',
+         DeliveryTimeListCreateView.as_view(),
+         name='delivery-time-list-create'),
+    path('delivery-times/<int:pk>/',
+         DeliveryTimeDetailView.as_view(),
+         name='delivery-time-detail'),
 
 ]
